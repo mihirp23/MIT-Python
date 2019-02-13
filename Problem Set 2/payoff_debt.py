02c37c75e56da4e80c71f3eb2c3d3f0f5438f6cc
mihirp23 <mihirp38661@gmail.com>
2019-02-13 12:50:46 -0500

    Implemented solution

--- Problem Set 2/payoff_debt.py
+++ Problem Set 2/payoff_debt.py
@@ -13,7 +13,17 @@
 ##################################################################
 def main():
 ##################################################################
-    print ("hello world")
+    balance = 42
+    annual_interest_rate = 0.2
+    monthly_payment_rate = 0.04
+    monthly_interest_rate = annual_interest_rate / 12
+    
+    for i in range (0, 12):
+        minimum_monthly_payment = monthly_payment_rate * balance
+        monthly_unpaid_balance = balance - minimum_monthly_payment
+        updated_balance = monthly_unpaid_balance + (monthly_interest_rate * monthly_unpaid_balance)
+        balance = updated_balance
+        print("Month " + str(i + 1) + " Remaining Balance: " + str(format(updated_balance, '.2f')))
 
 # main()
 
