997381a51c100606f36c77e31e491d60c158b2e5
mihirp23 <mihirp38661@gmail.com>
2019-02-14 08:28:41 -0500

    Implemented solution

--- Problem Set 2/payoff_debt_one_year.py
+++ Problem Set 2/payoff_debt_one_year.py
@@ -15,7 +15,31 @@
 ##################################################################
 def main():
 ##################################################################
-    print ("hello world")    
+    original_balance = 3926
+    annual_interest_rate= 0.2
+    monthly_interest_rate = annual_interest_rate / 12
+    monthly_payment = 0
+    keepCalculating = True
+    
+    while (keepCalculating):
+        
+        # keep incrementing by 10 to find the lowest fixed monthly payment
+        # since it has to be multiple of 10
+        
+        monthly_payment += 10
+        # we're trying again, so reset the balance value to orignal.
+        balance =  original_balance
+        
+        # let's see if this monthly payment is sufficent
+        for i in range(0, 12):
+            monthly_unpaid_balance = balance  - monthly_payment
+            balance = monthly_unpaid_balance + (monthly_interest_rate  * monthly_unpaid_balance)
+            
+            if balance <= 0:
+                keepCalculating = False
+                break
+            
+    print("Lowest payment: " + str(monthly_payment))
 
 # main()
 
