4fe3b438327d7c7ec13f5791b5204c65ef75b199
mihirp23 <mihirp38661@gmail.com>
2019-02-14 08:53:39 -0500

    Implemented solution

--- Problem Set 2/payoff_debt_using_bisection.py
+++ Problem Set 2/payoff_debt_using_bisection.py
@@ -17,7 +17,44 @@
 ##################################################################
 def main():
 ##################################################################
-    print ("hello world")    
+    original_balance = 999999
+    annual_interest_rate= 0.18
+    monthly_interest_rate = annual_interest_rate / 12
+    monthly_payment = 0
+    
+    
+    lower_bound = original_balance / 12
+    upper_bound = (original_balance * ((1 + monthly_interest_rate) ** 12)) / 12.0
+    
+    
+    
+    while (lower_bound <= upper_bound):
+        
+        # midpoint
+        monthly_payment = (lower_bound + upper_bound) / 2
+        
+        # set the balance variable to original value
+        balance =  original_balance
+        
+        # let's see if this monthly payment is sufficent
+        for i in range(0, 12):
+            monthly_unpaid_balance = balance  - monthly_payment
+            balance = monthly_unpaid_balance + (monthly_interest_rate  * monthly_unpaid_balance)
+        
+        print ("upper bound : " + str(upper_bound))
+        print ("lower bound : " + str(lower_bound))
+        print (balance)
+        
+        
+        # adjust our upper and lower bounds according to the calculated balance
+        if balance < 0:
+            upper_bound = monthly_payment
+            lower_bound = lower_bound + 1
+        else:
+            lower_bound = monthly_payment
+            upper_bound = upper_bound - 1
+        
+    print("Lowest payment: " + str(format(monthly_payment, '.2f')))    
 
 # main()
 
